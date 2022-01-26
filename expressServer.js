80e502373c7d9559ba0c27ffa051510404b561e0
Baktash Sana <baktash11@gmail.com>
2022-01-26 03:54:12 -0500

    fs moduel has been added to handel static file a

--- expressServer.js
+++ expressServer.js
@@ -1,5 +1,8 @@
 const http = require("http");
 
+// fs giving node access to this computer file system
+const fs = require("fs");
+
 // http req
 const server = http.createServer((req, res) => {
   console.log("request model");
@@ -7,24 +10,29 @@ const server = http.createServer((req, res) => {
   if (req.url === "/") {
     //header
     res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h2>Hello world</h2>");
+    // res.write("<h2>Hello world</h2>");
+    const homePageHTML = fs.readFileSync("index.html");
+
+    res.write(homePageHTML);
     res.end();
-
-  }else if (req.url === '/about') {
-      res.writeHead(200, {"content-type": "text/html"});
-      res.write("<h2> Baktash Sana </h2>" );
-      res.end();
-
-    
-      
-  } else if(req.url ==="/contact") {
-      res.writeHead(200, {'content-type': 'text/html'});
-      res.write('<p>email-address:  baktash@live.ca</p> </br> <p>contact-number: 9054421991 </p> ')
-      res.end();
-  }else{
-      res.writeHead(404, {'content-type': 'text/html'});
-      res.write('<h3> Sorry this page does not exist</h3>')
-      res.end();
+  } else if (req.url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h2> Baktash Sana </h2>");
+    res.end();
+  } else if (req.url === "/images.png") {
+    res.writeHead(200, { "content-type": "image/png" });
+    const image = fs.readFileSync("images.png");
+    res.write(image);
+    res.end();
+  } else if (req.url === "/style.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    const style = fs.readFileSync("style.css");
+    res.write(style);
+    res.end();
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h3> Sorry this page does not exist</h3>");
+    res.end();
   }
 });
 
