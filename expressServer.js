3c47067bdca81a1ac29e3c7bb033042259771c33
Baktash Sana <baktash11@gmail.com>
2022-01-26 02:46:39 -0500

    more contact about and error route has been created

--- expressServer.js
+++ expressServer.js
@@ -1,17 +1,31 @@
-const http = require ('http');
+const http = require("http");
 
 // http req
-const server = http.createServer((req, res)=>{
-    console.log("request model");
-    console.log(req);
-
-    //header 
-    res.writeHead(200, {'content-type':'text/html'});
-    res.write('<h2>Hello world</h2>')
+const server = http.createServer((req, res) => {
+  console.log("request model");
+  console.log(req);
+  if (req.url === "/") {
+    //header
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h2>Hello world</h2>");
     res.end();
 
+  }else if (req.url === '/about') {
+      res.writeHead(200, {"content-type": "text/html"});
+      res.write("<h2> Baktash Sana </h2>" );
+      res.end();
+
+    
+      
+  } else if(req.url ==="/contact") {
+      res.writeHead(200, {'content-type': 'text/html'});
+      res.write('<p>email-address:  baktash@live.ca</p> </br> <p>contact-number: 9054421991 </p> ')
+      res.end();
+  }else{
+      res.writeHead(404, {'content-type': 'text/html'});
+      res.write('<h3> Sorry this page does not exist</h3>')
+      res.end();
+  }
 });
 
-
-
-server.listen(3000, ()=>console.log('http://localhost:3000'));
+server.listen(3000, () => console.log("http://localhost:3000"));
